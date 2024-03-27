9ce181c0a4139975bcc07efdc27c737c6eaf0be1
jaxs-ribs <luc.baracat@googlemail.com>
2024-03-27 21:36:12 +0100

    Frontend shit

--- pkg/ui/buy/assets/index-u8NCL94_.js
+++ pkg/ui/buy/assets/index-u8NCL94_.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as W,a as C,o as ot,R as Uo,p as K,y as gt}from"./index-B3czRNtE.js";import"./index-wjd_LSlr.js";/**
+import{n as _t,s as ce,T as N,t as W,a as C,o as ot,R as Uo,p as K,y as gt}from"./index-oMDT38vO.js";import"./index-agxrSU0P.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
