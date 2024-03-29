427d1f74619e8a08b6e2ab3470cab74ee7e420e7
bitful-pannul <bitful-pancreas@protonmail.com>
2024-03-29 14:49:49 -0400

    sell_ui: default to hosted URL

--- pkg/ui/sell/assets/index-D8rF4zQh.js
+++ pkg/ui/sell/assets/index-D8rF4zQh.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as W,a as C,o as ot,R as Uo,p as K,y as gt}from"./index-BkKee2rB.js";import"./index-L9F9JQl2.js";/**
+import{n as _t,s as ce,T as N,t as W,a as C,o as ot,R as Uo,p as K,y as gt}from"./index-ChaKCw8L.js";import"./index-Bglcm_0e.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
